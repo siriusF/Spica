c16527032d57524cc94035a159423b560d890ed8
bawangxx <bawangxx@qq.com>
2016-03-09 18:54:12 +0800

    Initial Commit

---
+++ 宅舞/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  宅舞
+//
+//  Created by apple on 16/3/9.
+//  Copyright © 2016年 beizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 宅舞/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  宅舞
+//
+//  Created by apple on 16/3/9.
+//  Copyright © 2016年 beizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
